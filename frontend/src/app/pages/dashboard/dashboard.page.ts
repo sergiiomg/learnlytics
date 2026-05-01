d0551861c7549d0f6899fa4a051e7e5aab7f301f
Sergiio <sergiiomgdaw@gmail.com>
2026-05-01 10:56:59 +0200

    Crear tabs  y dashboard inicial

--- frontend/src/app/pages/dashboard/dashboard.page.ts
+++ frontend/src/app/pages/dashboard/dashboard.page.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { Auth } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonCard, IonCardHeader, IonCardTitle, IonCardContent, CommonModule, FormsModule]
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: Auth,
+    private router: Router
+  ) { }
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigateByUrl('/login');
+  }
 
   ngOnInit() {
   }
--- frontend/src/app/pages/tabs/tabs.page.ts
+++ frontend/src/app/pages/tabs/tabs.page.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { homeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel]
 })
 export class TabsPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({
+      homeOutline
+    });
+  }
 
   ngOnInit() {
   }
